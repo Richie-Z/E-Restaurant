1d4fd4b29538c4136f47e76345fd56944ebe7ff5
Richie-Z <zakariarichie@yahoo.com>
2020-12-04 13:29:34 +0700

    updated composer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeed::class);
+        $this->call(UserSeed::class);
         $this->call(Status::class);
     }
 }
